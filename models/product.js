b1f2c49cafe751c468f7243b7a868949c66be39f
iwai-0310 <chazesingh@gmail.com>
2023-12-15 20:03:31 +0530

    Adds schema for products for our FE-ecommerce

---
+++ models/product.js
@@ -0,0 +1,50 @@
+const mongoose=require('mongoose');
+
+const productSchema=new mongoose.Schema({
+  id:{
+    type:String,
+    required:true,
+  },
+  category:{
+    type:String,
+    required: [true,'product category must be defined']
+  },
+  name:{
+    type:String,
+    required:[true,'product name must be provided'],
+  },
+  seller:{
+    type:String,
+    required:[true,'Seller must be provided']
+  },
+  price:{
+    type:Number,
+    required:[true,'product price must be provided'],
+  },
+  stock:{
+    type:Number,
+    required:[true,'Stock quantity should be provided']
+  },
+  rating:{
+    type:Number,
+    default: 4,
+  },
+  ratingsCount:{
+    type:Number,
+    default: 2000,
+  },
+  img:{
+    type:[String],
+    required:[true,'Image for product is a must']
+  },
+  shipping:{
+    type:Number,
+    default: 3,
+  },
+  quantity:{
+    type:Number,
+    default:1,
+  }
+})
+
+module.exports=mongoose.model('Product',productSchema);
