4a2116dcb5449d559f406bdb989b182145de3999
iwai-0310 <chazesingh@gmail.com>
2023-12-15 23:05:44 +0530

    Adds support for mongoose query find on req params

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,30 @@
+//Get the model prodcutschema
+const Product=require('../models/product')
+
+//static section for testing
 const getAllProductsStatic=async (req,res)=>{
-  throw new Error('testing async errors')
-  res.status(200).json({msg: 'products testing route'})
+  //mongoose query the product schema to return all the items
+  const products=await Product.find({})
+  //set the response to return all the products store using find query
+  res.status(200).json({products, ngHits: products.length})
 }
 const getAllProducts=async (req,res)=>{
-  throw new Error('testing async errors')
-  res.status(200).json({msg: 'products route'})
+  //accessing the query paramters in console
+  // console.log(req.query)
+
+  //implement a better approach try to destructured the req query
+  const {seller} =req.query
+  //instead of directly passing it to the find its better to create a new object
+  const queryObject={}
+  if(seller){
+    queryObject.seller=seller==='Addidas'? 'Addidas' : 'Apple'
+  }
+  console.log(queryObject)
+  //pass the query directly into mongoose query
+  const products=await Product.find(
+    queryObject
+  )
+  res.status(200).json({products, ngHits: products.length})
 }
 
 module.exports={
