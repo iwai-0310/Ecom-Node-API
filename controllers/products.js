4d47246123768756c6af99e35eef2346669ccc61
iwai-0310 <chazesingh@gmail.com>
2023-12-16 20:27:54 +0530

    Adds find and partial match for name and category props

--- controllers/products.js
+++ controllers/products.js
@@ -4,7 +4,7 @@ const Product=require('../models/product')
 //static section for testing
 const getAllProductsStatic=async (req,res)=>{
   //mongoose query the product schema to return all the items
-  const products=await Product.find({})
+  const products=await Product.find({name:'adidas Z.N.E. 01 ANC True Wireless Earbuds'})
   //set the response to return all the products store using find query
   res.status(200).json({products, ngHits: products.length})
 }
@@ -13,11 +13,16 @@ const getAllProducts=async (req,res)=>{
   // console.log(req.query)
 
   //implement a better approach try to destructured the req query
-  const {seller} =req.query
+  const {name,category} =req.query
   //instead of directly passing it to the find its better to create a new object
   const queryObject={}
-  if(seller){
-    queryObject.seller=seller==='Addidas'? 'Addidas' : 'Apple'
+  //if name matches the name value in the req query and use regEx for partial matches
+  if(name){
+    queryObject.name={ $regex:name, $options: 'i'}
+  }
+  //if category matches with category in the req query and use regEx for partial matches
+  if(category){
+    queryObject.category={ $regex:category, $options: 'i'}
   }
   console.log(queryObject)
   //pass the query directly into mongoose query
