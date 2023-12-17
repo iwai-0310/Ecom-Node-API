0e16363b087212823b879af929fe6a946a1f6f64
iwai-0310 <chazesingh@gmail.com>
2023-12-18 02:29:10 +0530

    Adds support to add a product to a collection

--- controllers/products.js
+++ controllers/products.js
@@ -8,6 +8,16 @@ const getAllProductsStatic=async (req,res)=>{
   //set the response to return all the products store using find query
   res.status(200).json({products, ngHits: products.length})
 }
+
+const createTask=async (req,res)=>{
+  try {
+    const newProduct=await Product.create(req.body);
+    res.status(200).json({newProduct})
+  } catch (error) {
+    res.status(500).send({msg: error})
+  }
+}
+
 const getAllProducts=async (req,res)=>{
   //accessing the query paramters in console
   // console.log(req.query)
@@ -75,5 +85,5 @@ const getAllProducts=async (req,res)=>{
 }
 
 module.exports={
-  getAllProducts,getAllProductsStatic
+  getAllProducts,getAllProductsStatic,createTask
 }
--- routes/products.js
+++ routes/products.js
@@ -2,9 +2,9 @@ const express=require('express');
 
 const router=express.Router();
 
-const {getAllProducts,getAllProductsStatic}=require("../controllers/products");
+const {getAllProducts,getAllProductsStatic,createTask}=require("../controllers/products");
 
-router.route('/').get(getAllProducts)
+router.route('/').get(getAllProducts).post(createTask)
 router.route('/static').get(getAllProductsStatic)
 
 module.exports=router;
